9d02fcce20e9685a4ba95546f194d7e03d40a454
Diego T. Fialho <diego.tfialho@gmail.com>
2018-04-03 21:10:11 -0300

    mongoose configured and users schema added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
+
+const keys = require('./config/keys');
+
+mongoose.connect('mongodb://ds233769.mlab.com:33769/emaily-dev', {
+  user: keys.mongoUsername,
+  pass: keys.mongoPassword
+});
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy(
 	{
@@ -9,8 +12,6 @@ passport.use(new GoogleStrategy(
 		callbackURL: '/auth/google/callback'
 	},
 	(accessToken, refreshToken, profile, done) => {
-		console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
+		new User({ googleID: profile.id }).save();
 	}
 ));
