0d0393972c38ae58c5b84984999afbf65e5b7d8f
Diego T. Fialho <diego.tfialho@gmail.com>
2018-06-08 14:35:33 -0300

    Payments component

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
 	mongoUri: process.env.MONGO_URI,
 	mongoUsername: process.env.MONGO_USERNAME,
 	mongoPassword: process.env.MONGO_PASSWORD,
-	cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
